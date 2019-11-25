f1ef0db7151d820312571e26ee7672f216a7ecca
Ubuntu <ubuntu@ip-172-31-6-42.ec2.internal>
2019-11-25 09:33:55 +0000

    I am pushing all my files to my GitHub

---
+++ C-Program.c
@@ -0,0 +1,7 @@
+#include<studio.h>
+int main()
+{
+printf("Hello I am C-Program. Thank You!");
+return 0;
+}
+
---
+++ HelloJava.java
@@ -0,0 +1,6 @@
+class HelloJava{
+public static void mani(String args[])
+{
+System.out.Println("I am your Java program. Thank You!");
+}
+}
